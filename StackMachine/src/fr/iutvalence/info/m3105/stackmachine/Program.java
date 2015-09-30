e52c85371d5d2416ce4c769ef1cf6133206b8286
Emmanuel LIONET <emmanuel-lionet@hotmail.fr>
2015-09-30 18:32:20 +0200

    Files added (fin de la première séance de TP)

--- StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Program.java
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Program.java
@@ -2,6 +2,10 @@ package fr.iutvalence.info.m3105.stackmachine;
 
 public class Program {
 
+	public Program(Instruction[] instructions) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public Instruction getInstructionAt(int instructionNumber) {
 		// TODO Auto-generated method stub
 		return null;
